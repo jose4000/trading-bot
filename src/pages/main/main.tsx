37eafe670532877d676c3fedc041535e8270affe
jose4000 <josemwa300@gmail.com>
2026-08-23 19:28:35 +0300

    fix: rename analysis to analysis tool

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -495,7 +495,7 @@ const AppWrapper = observer(() => {
                                             width='24px'
                                             fill='var(--text-general)'
                                         />
-                                        <Localize i18n_default_text='Analysis' />
+                                        <Localize i18n_default_text='Analysis Tool' />
                                     </>
                                 }
                                 id='id-analysis'
